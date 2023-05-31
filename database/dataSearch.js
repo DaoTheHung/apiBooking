f839bda8999311f178db9ad31817ae217f5aafd6
DaoTheHung <Daohung447@gmail.com>
2023-05-31 23:42:53 +0700

    firts commit

--- database/dataSearch.js
+++ database/dataSearch.js
@@ -1,4 +1,5 @@
 const dataB = [
+    // Chuyên Khoa
     {
         id: 1,
         title: "Phẫu thuật Thận - Tiết niệu",
@@ -15,30 +16,87 @@ const dataB = [
         image: "https://cdn.bookingcare.vn/fr/w300/2023/02/02/144538-5.png"
     },
     {
+        id: 4,
+        title: "Gói phẫu thuật mắt bằng phương pháp PHAKIC tại Bệnh viện Mắt Thiên Thanh",
+        image: "https://cdn.bookingcare.vn/fr/w200/2023/02/01/105629-1logo-bv-mat-thien-thanh.jpg"
+    },
+    {
         id: 5,
-        title: "Bệnh viện Hữu nghị Việt Đức",
-        image: "https://cdn.bookingcare.vn/fr/w500/2020/06/03/114348-bv-viet-duc.jpg"
+        title: "Phẫu thuật Nam học",
+        image: "https://cdn.bookingcare.vn/fr/w300/2023/02/02/144537-2.png"
     },
     {
         id: 6,
-        title: "Bệnh viện Chợ Rẫy",
-        image: "https://cdn.bookingcare.vn/fr/w500/2021/09/14/095119-benh-vien-cho-ray-h1.jpg"
+        title: "Bác sĩ gia đình",
+        image: "https://cdn.bookingcare.vn/fr/w300/2023/02/02/144537-2.png"
     },
     {
         id: 7,
+        title: "Bác sĩ Cơ-Xương-Khớp từ xa",
+        image: "https://cdn.bookingcare.vn/fr/w300/2019/02/27/165326kham-benh-co-xuong-khop-1.jpg"
+    },
+    {
+        id: 8,
+        title: "Bác sĩ Tiêu hóa từ xa",
+        image: "https://cdn.bookingcare.vn/fr/w300/2018/01/17/171636bac-si-tieu-hoa.jpg"
+    },
+    {
+        id: 9,
+        title: "Bác sĩ Nội khoa từ xa",
+        image: "https://cdn.bookingcare.vn/fr/w300/2019/12/13/120741-tim-mach.jpg"
+    },
+    {
+        id: 10,
+        title: "Bác sĩ Nam học từ xa",
+        image: "https://cdn.bookingcare.vn/fr/w300/2018/10/16/151254nam-hoc.jpg "
+    },
+    {
+        id: 11,
+        title: "Bác sĩ Sản phụ khoa từ xa",
+        image: "https://cdn.bookingcare.vn/fr/w300/2020/07/17/085420-dia-chi-kham-san-phu-khoa-ha-noi.jpg "
+    }, {
+        id: 12,
+        title: "Bác sĩ Tim mạch từ xa",
+        image: "https://cdn.bookingcare.vn/fr/w300/2019/05/31/170831kham-tim-mach-4.jpg"
+    }, {
+        id: 13,
+        title: "Bác sĩ Nhi từ xa",
+        image: "https://cdn.bookingcare.vn/fr/w300/2017/12/19/173328kham-nhi-khoa.jpg"
+    }, {
+        id: 14,
+        title: "Bác sĩ Tai-Mũi-Họng từ xa",
+        image: "https://cdn.bookingcare.vn/fr/w300/2018/08/18/095722tai-mui-hong.jpg"
+    }, {
+        id: 15,
+        title: "Bác sĩ Thần kinh từ xa",
+        image: "https://cdn.bookingcare.vn/fr/w300/2019/10/03/100739bac-si-than-kinh.jpg"
+    },
+    {
+        id: 16,
         title: "Phòng khám Bệnh viện Đại học Y Dược 1",
         image: "https://cdn.bookingcare.vn/fr/w500/2020/05/29/112414-pk-dhyd1.jpg"
     },
     {
-        id: 8,
-        title: "Trung tâm Khám sức khỏe định kỳ",
+        id: 17,
+        title: "Trung tâm Khám sức khỏe định kỳ, Bệnh viện Trung ương Quân đội 108",
         image: "https://cdn.bookingcare.vn/fr/w500/2023/01/11/163604-223000085957trung-tam-kham-suc-khoe-dinh-ky-108.jpg"
     },
     {
-        id: 9,
+        id: 18,
         title: "Bệnh viện Ung bướu Hưng Việt",
         image: "https://cdn.bookingcare.vn/fr/w500/2020/06/04/095412-bv-hung-viet.jpg"
     },
+    {
+        id: 19,
+        title: "Phó Giáo sư, Tiến sĩ, Bác sĩ cao cấp Nguyễn Duy Hưng",
+        image: "https://cdn.bookingcare.vn/fr/w200/2020/03/17/114430-bshung.jpg"
+    },
+    {
+        id: 20,
+        title: "Bác sĩ Chuyên khoa II Trần Minh Khuyên",
+        image: "https://cdn.bookingcare.vn/fr/w200/2021/01/18/105401-bsckii-tran-minh-khuyen.jpg"
+    },
+    //
 
 
 
